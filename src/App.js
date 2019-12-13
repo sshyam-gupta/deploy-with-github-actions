57632668efb737d4fd06cf4f52f962b2afe60414
Shyam Gupta <shyamsundar.g@kiprosh.com>
2019-12-13 17:07:41 +0530

    Update config

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Deploy with github action
+          react react
         </a>
       </header>
     </div>
