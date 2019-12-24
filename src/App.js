b6945052da6a7cede1a69f945b1ddd0044b7b22f
Shyam Gupta <shyamsundar.g@kiprosh.com>
2019-12-24 13:09:15 +0530

    Re test action

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Working with react
+          Working with react 1
         </a>
       </header>
     </div>
