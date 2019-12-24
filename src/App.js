8eec506068af4eea8e84fc4688ffbbd0327c30e0
Shyam Gupta <shyamsundar.g@kiprosh.com>
2019-12-24 15:18:04 +0530

    CLEAN true

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Working with react 3
+          Working with react 4
         </a>
       </header>
     </div>
